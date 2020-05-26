9f12c9a0d3eccf4143c735869b6859fb9ee9ff06
tdn <hatano0300@gmail.com>
2020-05-26 22:59:53 +0900

    Editing book

---
+++ src/main/java/jp/tdn/japanese_food_mod/world/JPGeneration.java
@@ -0,0 +1,9 @@
+package jp.tdn.japanese_food_mod.world;
+
+public class JPGeneration {
+    public static void setup(){
+        GrassGeneration.setupGrassGeneration();
+        OreGeneration.setupOreGeneration();
+        OysterGeneration.setupOysterGeneration();
+    }
+}
