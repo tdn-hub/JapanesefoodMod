c16a41811e53b50e598e9237f237437f0f7c9c29
tdn <hatano0300@gmail.com>
2020-07-29 11:36:56 +0900

    Make accessTransfomer and fix loot table loading.

--- src/main/java/jp/tdn/japanese_food_mod/events/LootLoadEventHandler.java
+++ src/main/java/jp/tdn/japanese_food_mod/events/LootLoadEventHandler.java
@@ -3,6 +3,7 @@ package jp.tdn.japanese_food_mod.events;
 import jp.tdn.japanese_food_mod.JapaneseFoodMod;
 import jp.tdn.japanese_food_mod.config.FishingConfig;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootEntry;
 import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.TableLootEntry;
 import net.minecraftforge.event.LootTableLoadEvent;
@@ -37,8 +38,21 @@ public class LootLoadEventHandler {
         }
 
         if(FishingConfig.fishing_overworld.get() && event.getName().equals(fish)){
-            event.getTable().removePool("main");
-            event.getTable().addPool(LootPool.builder().addEntry(TableLootEntry.builder(new ResourceLocation(JapaneseFoodMod.MOD_ID, "gameplay/fishing"))).build());
+//            event.getTable().removePool("main");
+//            event.getTable().addPool(LootPool.builder().addEntry(TableLootEntry.builder(new ResourceLocation(JapaneseFoodMod.MOD_ID, "gameplay/fishing"))).build());
+            LootPool pool = event.getTable().getPool("main");
+            addEntry(pool, getInjectEntry(new ResourceLocation(JapaneseFoodMod.MOD_ID, "gameplay/fishing/fish"), 85, -1));
         }
+    }
+
+    private static LootEntry getInjectEntry(ResourceLocation location, int weight, int quality) {
+        return TableLootEntry.builder(location).weight(weight).quality(quality).build();
+    }
+
+    private static void addEntry(LootPool pool, LootEntry entry) {
+        if (pool.lootEntries.stream().anyMatch(e -> e == entry)) {
+            throw new RuntimeException("Attempted to add a duplicate entry to pool: " + entry);
+        }
+        pool.lootEntries.add(entry);
     }
 }
